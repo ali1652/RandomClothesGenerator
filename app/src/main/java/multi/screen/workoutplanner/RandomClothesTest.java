99ab72eab68ee083359a521acc730202a5c90f26
ali1652 <123775876+ali1652@users.noreply.github.com>
2023-03-22 21:27:33 +0000

    input/ retrieve from firebase
    Created input button and implemented retrieving data from firebase

--- app/src/main/java/multi/screen/workoutplanner/RandomClothesTest.java
+++ app/src/main/java/multi/screen/workoutplanner/RandomClothesTest.java
@@ -13,9 +13,11 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,46 +35,67 @@ public class RandomClothesTest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random_clothes_test);
 
-         shirts = findViewById(R.id.shirtsInput);
+        shirts = findViewById(R.id.shirtsInput);
 
-         String getShirtsInput = shirts.getText().toString();
+        String getShirtsInput = shirts.getText().toString();
 
         String Tshirts[]
-                = { "Nike Blue", "Primark red",
+                = {"Nike Blue", "Primark red",
                 "Adidas white"};
+
+        userShirts = new ArrayList<>();
+
 
         ListView listView = findViewById(R.id.listview);
         //sets the view of list
         // ArrayAdapter<String> arrayList = getList();
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,Tshirts);
-        //l.setChoiceMode(textview.Choice);
-        listView.setAdapter(arrayAdapter);
-        //listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
-        userShirts = new ArrayList<>();
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, userShirts);
+        ;
+        listView.setAdapter(arrayAdapter);
+
+
+        //referencing the database
         database = FirebaseDatabase.getInstance("https://workoutplanner-49f96-default-rtdb.europe-west1.firebasedatabase.app/");
         reference = database.getReference().child("Shirts");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    userShirts.add(snapshot.getValue().toString());
+                }
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
     }
 
+
     // adds data to the firebase database once user has inputted a shirt
+
     public void buttonClick(View view) {
         String getShirtsInput = shirts.getText().toString();
-        reference.push().setValue(getShirtsInput)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(RandomClothesTest.this, "Shirt added", Toast.LENGTH_SHORT).show();
+                        if (getShirtsInput.isEmpty()) {
+                            Toast.makeText(RandomClothesTest.this, "Enter value", Toast.LENGTH_SHORT).show();
                         } else {
-                            Toast.makeText(RandomClothesTest.this, "Failed to add shirt to database", Toast.LENGTH_SHORT).show();
+                            reference.push().setValue(getShirtsInput);
+                            Toast.makeText(RandomClothesTest.this, "Shirt added", Toast.LENGTH_SHORT).show();
                         }
-                    }
-                });
 
     }
 
 }
+
+
+
 
 
